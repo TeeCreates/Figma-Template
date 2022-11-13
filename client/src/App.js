482b25d57cc4ef9fad276020122f73953185be76
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2022-11-13 11:19:33 -0500

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// ^^ this can be used when you want the links to connect to another page
+import SideBar from "./SideBar";
+import styled from "styled-components";
+import GlobalStyle from "./globalStyles";
+import { Fragment } from "react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <SideBar />
+    </Fragment>
+  );
 }
 
 export default App;
+
+const Content = styled.div`
+  background-color: lightblue;
+  padding: 10px;
+`;
